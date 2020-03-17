16c25a09047c1d5bb587f7652ef90cf1ba4b76ae
Dylan Hautecoeur <dylan.hautecoeur@gmail.com>
2020-03-17 16:47:57 +0100

    Added somes files

--- classes/Requete.php
+++ classes/Requete.php
@@ -14,7 +14,22 @@ class Requete {
   }
 
   public function inserer($table, $valeurs) {
-    INSERT INTO $table() VALUES($valeurs[])
+    $sql = "INSERT INTO $table ";
+    foreach($values as $key => $value) {
+      $sql .= $key;
+      if($key !== count($value - 1)) {
+        $sql .= ", ";
+      }
+    }
+
+    $sql = "INSERT INTO VALUES ";
+
+      foreach ($values as $key => $content) {
+        $sql .= $content[0];
+        if ($key !== count($values) - 1) {
+          $sql .=", ";
+        }
+      }
   }
 
   public function __destruct() {
--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
 date_default_timezone_set('Europe/Paris');
-require_once './functions/classAutoLoader.php';
+require_once './functions/classAutoLoad.php';
 spl_autoload_register('classAutoLoader');
 
-$valeurs = new Array(
-  'valeur1' => 'nom';
-  'valeur2' => 'prenom';
-  'valeur3' => 'email';
-  'valeur4' => 'motdepasse';
+$valeurs = array(
+  'nom' => 'Michel',
+  'prenom' => 'Jean-Pierre',
+  'email' => 'jp@michel.com',
+  'motdepasse' => 'michelalaplage'
 );
+
+//On instancie un objet de la classe Requête
+$test = new Requete();
+
+// C'est l'occasion de tester notre classe d'écriture d'erreur
+var_dump($test);
